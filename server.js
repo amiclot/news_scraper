e376f4852c08bbcff55ce9a1080cf7dad2a664f3
Adam Miclot <amiclot09@gmail.com>
2017-11-20 10:24:55 -0500

    heroku not working, no errors

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.set("view engine", "handlebars");
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/yungjj_news_scrapper", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/yungjj_news_scrapper";
+
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
